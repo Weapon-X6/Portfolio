f866a2ecfbe4edfc6bd56b78df5aaf677b882957
waffe_x20 <argueta_eduardo@yahoo.de>
2023-08-04 14:47:44 -0600

    Add draft template

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def project_list(request):
-    return HttpResponse("<h2>RP</h2>")
+    return render(request, "projects/index.html")
